b82583714793c7ccce3ecea3a31bba7605ce1937
Alexey Lapshin <alexey.lapshin@espressif.com>
2025-06-02 12:53:28 +0700

    RISCV: add esp-base tune option

--- gcc/config/riscv/riscv.cc
+++ gcc/config/riscv/riscv.cc
@@ -466,6 +466,28 @@ static const struct riscv_tune_param rocket_tune_info = {
   NULL,						/* loop_align */
 };
 
+/* Costs to use when optimizing for esp-base. Copied from rocket.  */
+static const struct riscv_tune_param esp_base_tune_info = {
+  {COSTS_N_INSNS (4), COSTS_N_INSNS (5)},	/* fp_add */
+  {COSTS_N_INSNS (4), COSTS_N_INSNS (5)},	/* fp_mul */
+  {COSTS_N_INSNS (20), COSTS_N_INSNS (20)},	/* fp_div */
+  {COSTS_N_INSNS (4), COSTS_N_INSNS (4)},	/* int_mul */
+  {COSTS_N_INSNS (33), COSTS_N_INSNS (65)},	/* int_div */
+  1,						/* issue_rate */
+  3,						/* branch_cost */
+  5,						/* memory_cost */
+  8,						/* fmv_cost */
+  false,					/* slow_unaligned_access */
+  false,					/* vector_unaligned_access */
+  false,					/* use_divmod_expansion */
+  false,					/* overlap_op_by_pieces */
+  RISCV_FUSE_NOTHING,                           /* fusible_ops */
+  NULL,						/* vector cost */
+  NULL,						/* function_align */
+  NULL,						/* jump_align */
+  NULL,						/* loop_align */
+};
+
 /* Costs to use when optimizing for Sifive 7 Series.  */
 static const struct riscv_tune_param sifive_7_tune_info = {
   {COSTS_N_INSNS (4), COSTS_N_INSNS (5)},	/* fp_add */
